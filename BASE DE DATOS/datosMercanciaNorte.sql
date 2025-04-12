e5c890df1998fc770792001b8fbd4b786679c288
juanpmqz18 <161894066+juanpmqz18@users.noreply.github.com>
2025-04-12 18:59:08 -0500

    Create datosMercanciaNorte.sql

---
+++ BASE DE DATOS/datosMercanciaNorte.sql
@@ -0,0 +1,11 @@
+INSERT INTO Mercancia (id_mercancia, descripcion, peso, valor_estimado, id_tipo_mercancia, id_cliente, id_viaje, id_boleto) VALUES
+(001, 'Caja con productos electrónicos para tienda', 15.50, 50000.00, 001, 001, 001, 001),   
+(002, 'Lotes de ropa para mayorista', 100.00, 150000.00, 002, 002, 002, 002),  
+(003, 'Cajas con repuestos automotrices', 200.00, 120000.00, 003, 003, 003, 003),  
+(004, 'Muebles pequeños para mudanza', 60.00, 85000.00, 004, 004, 004, 004),  
+(005, 'Alimentos no perecederos para supermercado', 50.00, 45000.00, 005, NULL, 005, 005),   
+(006, 'Paquete de materiales para construcción', 300.00, 200000.00, 006, NULL, 006, 006),   
+(007, 'Electrodomésticos pequeños para tienda', 20.00, 75000.00, 007, 005, 007, 007), 
+(008, 'Productos artesanales para exportación', 25.00, 60000.00, 008, 006, 008, 008),  
+(009, 'Ropa de cama y toallas para hotel', 10.00, 30000.00, 009, 007, 009, 009),  
+(010, 'Juguetes de fabricación nacional', 40.00, 100000.00, 010, NULL, 010, 010);  
